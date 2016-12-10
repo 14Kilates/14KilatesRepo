55d827be105180911363de1bfaf2d6af49424bde
14kilates <addonteam@14kilates.net>
2016-12-10 17:47:37 -0400

    14 Kilates
    Prueba

---
+++ plugin.video.14K/weblogin.py
@@ -0,0 +1,54 @@
+import os
+import re
+import urllib,urllib2
+import cookielib
+
+def check_login(source,username):
+    logged_in_string = 'svn/trunk'
+    if re.search(logged_in_string,source,re.IGNORECASE):
+        return True
+    else:
+        return False
+
+
+def doLogin(cookiepath, username, password):
+
+    if not os.path.isfile(cookiepath):
+        cookiepath = os.path.join(cookiepath,'cookies.lwp')
+
+    try:
+        os.remove(cookiepath)
+    except:
+        pass
+
+    if username and password:
+
+        login_url = 'http://addon14kilates.com/secure/autentify.php'
+
+        header_string = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3'
+
+        login_data = urllib.urlencode({'access_login':username, 'access_password':password, 'Submit':'Submit'})
+
+        req = urllib2.Request(login_url, login_data)
+        req.add_header('User-Agent',header_string)
+
+        cj = cookielib.LWPCookieJar()
+
+        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+
+        response = opener.open(req)
+        source = response.read()
+        response.close()
+
+        login = check_login(source,username)
+
+        if login == True:
+            cj.save(cookiepath)
+            tvpath = source
+        elif login == False:
+            tvpath = False
+
+        return tvpath
+    
+    else:
+        return False
